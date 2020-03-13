79d022c87db48c15b1c27af2bbcc01ba636ee1b5
Gerardo Coss Alcantar <gcoss@cinepolis.com>
2020-03-13 14:53:19 -0600

    [add] animeflv

--- script.js
+++ script.js
@@ -2,25 +2,28 @@ const puppeteer = require('puppeteer')
 const fs = require('fs')
 
 async function run() {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({headless: true})
     const page = await browser.newPage()
     let reviews = []
     async function getPageData(pageNumber = 1) {
-        await page.goto(`https://platzi.com/cursos/html5-css3/opiniones/${pageNumber}/`)
+        await page.goto(`https://animeflv.net/browse?order=title&page=${pageNumber}`)
+        if(pageNumber==1){
+            await page.waitFor(10000);
+        }
         // await page.screenshot({
         //     path: 'screenshot.png',
         //     fullPage: true,
         // })
         const data = await page.evaluate(() => {
-            const $reviews = document.querySelectorAll('.Review')
-            const $pagination = document.querySelectorAll('.Pagination .Pagination-number')
-            const totalPages = Number($pagination[$pagination.length - 1].textContent.trim())
+            const $reviews = document.querySelectorAll('li>article')
+            const $pagination = document.querySelectorAll('li>a[href*=page]')
+            const totalPages = Number($pagination[$pagination.length - 2].textContent.trim())
             const data = []
             $reviews.forEach(($review) => {
                 data.push({
-                    username: $review.querySelector('.Review-name').textContent,
-                    rating: $review.querySelector('.Review-stars .fulled').length,
-                    content: $review.querySelector('.Review-description').textContent.trim()
+                    content: $review.querySelector('h3').textContent,
+                    // rating: $review.querySelector('.Review-stars .fulled').length,
+                    // content: $review.querySelector('.Review-description').textContent.trim()
                 })
             })
             return {
@@ -30,7 +33,7 @@ async function run() {
         })
         reviews = [...reviews, ...data.reviews]
         console.log(`page ${pageNumber} of ${data.totalPages} completed`);
-        if(pageNumber <= data.totalPages - 130){
+    if(pageNumber <= data.totalPages){
             getPageData(pageNumber + 1)
         }else{
             fs.writeFile('data.js', `export default ${JSON.stringify(reviews)}`,()=>{
